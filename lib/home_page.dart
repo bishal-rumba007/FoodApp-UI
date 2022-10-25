b717a001807dd8b1763e78a47e1ebbc32f289527
bishal.rumba007 <bishal.rumba007@gmail.com>
2022-10-25 15:26:39 +0545

    home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'model/food.dart';
+
 class HomePage extends StatelessWidget {
 
   final searchController = TextEditingController();
@@ -51,6 +53,9 @@ class HomePage extends StatelessWidget {
             child: Container(
               child: TextField(
                   controller: searchController,
+                  onTap: (){
+                    searchController.clear();
+                  },
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     contentPadding: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
@@ -71,27 +76,59 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Container(
-            height: 160,
+            height: 170,
             width: double.infinity,
             child: ListView.builder(
+              itemCount: foods.length,
               scrollDirection: Axis.horizontal,
-                 itemCount: 10,
                 itemBuilder: (context, index) {
+                final food = foods[index];
                   return Padding(
                     padding: EdgeInsets.only(right: 10),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: Card(
-                        color: Colors.red,
-                        elevation: 5,
-                        shadowColor: Colors.black45,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 100,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      color: Colors.red,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 100,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                      height: 70,
+                                      width: 70,
+                                      child: Image.network(food.imageUrl)
+                                  ),
+                                  Text(food.foodName,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        shape: BoxShape.circle
+                                    ),
+                                    height: 30,
+                                    width: 30,
+                                    child: IconButton(
+                                        iconSize: 16,
+                                        onPressed: (){},
+                                        icon: Icon(Icons.arrow_forward_ios)
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   );
---
+++ lib/model/food.dart
@@ -0,0 +1,42 @@
+final foodData = [
+  {
+    'foodName': 'Burger',
+    'imageUrl': 'https://img.freepik.com/premium-vector/delicious-burger-icon-food-beverages_22052-1.jpg',
+  },
+  {
+    'foodName': 'Pizza',
+    'imageUrl': 'https://thumbs.dreamstime.com/b/slice-pizza-icon-cartoon-style-isolated-vector-illustration-88454426.jpg',
+  },
+  {
+    'foodName': 'Cake',
+    'imageUrl': 'https://img.freepik.com/premium-vector/birthday-cake-icon-cartoon-design_161751-2438.jpg?w=2000',
+  },
+  {
+    'foodName': 'Hot Dog',
+    'imageUrl': 'https://i.pinimg.com/474x/0a/94/3a/0a943a991cfbf792a239c255760807bf.jpg',
+  },
+  {
+    'foodName': 'Fries',
+    'imageUrl': 'https://cdn3.vectorstock.com/i/1000x1000/89/57/french-fries-icon-on-white-background-for-graphic-vector-23388957.jpg',
+  },
+];
+
+
+List<Food> foods = foodData.map((e) => Food.fromJson(e)).toList();
+
+class Food{
+  final String foodName;
+  final String imageUrl;
+
+  Food({
+    required this.foodName,
+    required this.imageUrl
+  });
+
+  factory Food.fromJson(Map<String, dynamic> json){
+    return Food(
+        foodName: json['foodName'],
+        imageUrl: json['imageUrl']
+    );
+  }
+}
